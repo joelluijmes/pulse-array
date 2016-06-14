d1064e5e7556c158fc00ed8504bf81a2d6fd56b4
Joel Luijmes <joelluijmes@gmail.com>
2016-06-14 17:46:54 +0200

    changed to ui.router

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,19 @@
-angular.module('myApp', ['ngRoute'])
-    .config(function ($routeProvider, $locationProvider) {
+angular.module('myApp', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider.otherwise('/home');
         $locationProvider.html5Mode(true);
 
-        $routeProvider
-            .when('/home', {templateUrl: '../partials/home.html'})
-            .when('/realtime', {templateUrl: '../partials/realtime.html'})
-            .when('/history', {templateUrl: '../partials/history.html'})
-            .when('/backup', {templateUrl: '../partials/backup.html'})
-            .when('/settings', {templateUrl: '../partials/settings.html'})
-            .otherwise({redirectTo: '/home'});
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '../partials/home.html'
+            })
+            .state('realtime', {
+                url: '/realtime',
+                templateUrl: '../partials/realtime.html'
+            })
+            .state('history', {
+                url: '/history',
+                templateUrl: '../partials/history.html'
+            });
     });
