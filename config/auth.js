0fe2547b35aad4e5c94c0380de6ac2f41c832393
Joel Luijmes <joelluijmes@gmail.com>
2016-06-15 20:00:39 +0200

    fixed partial white space views

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'googleAuth': {
-        'clientID'      : '841654215518-3najhn6rn5m1s80b0iiscof0e7t6mjee.apps.googleusercontent.com',
-        'clientSecret'  : 'UF3Iirzqm8wMlsPBsb3iKtP3',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
-    }
-};
