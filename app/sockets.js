c867468dc3ecda0db1c4d54189ca2ec61eada856
Joel Luijmes <joelluijmes@gmail.com>
2016-06-16 21:55:24 +0200

    passport.socketio

--- app/sockets.js
+++ app/sockets.js
@@ -1,12 +1,22 @@
 var socketio = require('socket.io');
+
+var server = null;
 var io = null;
 
-module.exports = {
-    broadcast: function(name, data) {
-        if (io)
-            io.sockets.emit(name, data);
-    },
-    startServer: function (app) {
-        io = socketio.listen(app);
-    }
+function onConnection(socket) {
+    console.log("connection");
+}
+
+function broadcast(name, data) {
+    io.sockets.emit(name, data);
+}
+
+
+module.exports = {};
+module.exports.startServer = function (app) {
+    io = socketio.listen(app);
+
+    io.on('connection', onConnection);
 };
+
+module.exports.broadcast = broadcast;
--- server.js
+++ server.js
@@ -9,19 +9,21 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
 var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var passportSocketIo = require('passport.socketio');
 
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var path = require('path');
+var MongoStore = require('connect-mongo')(session);
 
 var configDB = require('./config/database.js');
 var sockets = require('./app/sockets.js');
+var mongoStore = new MongoStore({url: configDB.url});
 
 // configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
-
 // set up our express application
 app.use(cookieParser());                                // read cookies (needed for auth)
 app.use(bodyParser.json());                             // html forms
@@ -40,7 +42,13 @@ app.set('views', viewsPath);
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({secret: 'joelandjasperissupercooledev:D', resave: true, saveUninitialized: true})); // session secret
+app.use(session({
+    key: 'express.sid',
+    secret: 'joelandjasperissupercooledev',
+    resave: true,
+    saveUninitialized: true,
+    store: mongoStore
+}));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -49,6 +57,37 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 require('./app/api')(app);
 require('./app/routes.js')(app, passport);
 require('./config/passport')(passport);
+
+// socketio ====================================================================
+//With Socket.io >= 1.0
+// io.use(passportSocketIo.authorize({
+//     cookieParser: cookieParser,       // the same middleware you registrer in express
+//     key: 'express.sid',       // the name of the cookie where express/connect stores its session_id
+//     secret: 'joelandjasperissupercooledev',    // the session_secret to parse the cookie
+//     store: mongoStore,        // we NEED to use a sessionstore. no memorystore please
+//     success: onAuthorizeSuccess,  // *optional* callback on success - read more below
+//     fail: onAuthorizeFail     // *optional* callback on fail/error - read more below
+// }));
+
+
+function onAuthorizeSuccess(data, accept) {
+    console.log('successful connection to socket.io');
+
+    // The accept-callback still allows us to decide whether to
+    // accept the connection or not.
+    accept(null, true);
+
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+    if (error)
+        throw new Error(message);
+    console.log('failed connection to socket.io:', message);
+
+    // We use this callback to log all of our failed connections.
+    accept(null, false);
+
+}
 
 // launch ======================================================================
 sockets.startServer(server);
